1047a91a0c9f5d966e37e19f6c95e3d4357c51f4
BenasMot <benasmotiejunas.bm@gmail.com>
2023-02-21 07:46:35 +0200

    remove value from description

--- src/hooks/useElements.ts
+++ src/hooks/useElements.ts
@@ -1,5 +1,6 @@
 import { ELEMENTS } from "@/constants/periodicTable";
-import { QuizElement } from "@/types/Element";
+import { Element, QuizElement, QuizElementValue } from "@/types/Element";
+import { capitalize } from "@/utils/capitalize";
 import { getRandomElements } from "@/utils/getRandomElements";
 import { useMemo } from "react";
 
@@ -11,7 +12,26 @@ export const useElements = (type: keyof QuizElement, ...deps: any[]) => {
     const correctValue = elements[0][type];
     const otherValues = elements.slice(1).map((el) => el[type]);
 
-    return { element: elements[0], correctValue, otherValues };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    const element = removeValueFromElementSummary(elements[0], correctValue);
+
+    return { element, correctValue, otherValues };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [type, ...deps]);
+};
+
+const removeValueFromElementSummary = (
+  element: Element,
+  value: QuizElementValue,
+) => {
+  const summary = element.summary;
+  if (value) {
+    const valStr = value.toString();
+    const regxp = RegExp(
+      `(${valStr})|(${capitalize(valStr)})|(${valStr.toLowerCase()})`,
+      "g",
+    );
+    const newSummary = summary.replaceAll(regxp, "<...>");
+    return { ...element, summary: newSummary };
+  }
+  return element;
 };
